ca2de4ae0f7a2d01336f04712934ee2575420211
anet-eny <anet.eny@gmail.com>
2025-12-13 19:34:03 +0100

    feat: update text-presets and utilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 function App() {
-  return (
-    <>
-      <h1 className="text-h1">hello world</h1>
-      <h2 className="text-h2">Hello world</h2>
-      <h3 className="text-h3">Hello world</h3>
-      <h4 className="text-h4">Hello world</h4>
-      <p className="text-body">Hello world</p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import iconMenu from "../assets/icon-hamburger.svg";
+
+export default function Header() {
+  return <header></header>;
+}
