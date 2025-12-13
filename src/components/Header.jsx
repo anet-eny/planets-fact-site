e6f8f0dcc23fd4b31d0de05524a0572e8d66bc70
anet-eny <anet.eny@gmail.com>
2025-12-13 20:04:58 +0100

    feat: add font imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 import iconMenu from "../assets/icon-hamburger.svg";
 
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <h1 className="text-preset-4 uppercase">the planets</h1>
+    </header>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
 
+import "@fontsource/antonio/500.css"; // Medium
+import "@fontsource/spartan/400.css"; // Regular
+import "@fontsource/spartan/700.css"; // Bold
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
