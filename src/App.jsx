803f0ace51c1182d3e287153acde7438c232b491
anet-eny <anet.eny@gmail.com>
2025-12-13 14:33:43 +0100

    feat: add design system theme and utilities, add assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <h1 className="text-h1">hello world</h1>
+      <h2 className="text-h2">Hello world</h2>
+      <h3 className="text-h3">Hello world</h3>
+      <h4 className="text-h4">Hello world</h4>
+      <p className="text-body">Hello world</p>
     </>
   );
 }
