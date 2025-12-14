825d56021427f33f7319c215471a2ff8829f0048
anet-eny <anet.eny@gmail.com>
2025-12-14 19:34:20 +0100

    feat: add current planet and tab logic, add handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import planetsData from "./data/planetsData";
 
 function App() {
+  const [selectedPlanet, setSelectedPlanet] = useState("earth");
+  const [activeTab, setActiveTab] = useState("overview");
+
+  const currentPlanet = planetsData[selectedPlanet];
+  const currentContent = currentPlanet[activeTab];
+
+  const handlePlanetChange = (planetName) => {
+    setSelectedPlanet(planetName);
+    setActiveTab("overview");
+  };
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <>
-      <Header />
+      <Header
+        planets={Object.keys(planetsData)}
+        selectedPlanet={selectedPlanet}
+        onPlanetChange={handlePlanetChange}
+      />
     </>
   );
 }
