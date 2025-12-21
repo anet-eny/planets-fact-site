c8e1724c6adced176f4103ddcfce8972ed60f71f
anet-eny <anet.eny@gmail.com>
2025-12-21 20:22:51 +0100

    feat: update styles of PlanetInfo, create global padding constant and use it in PlanetInfo div

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import TabButtons from "./components/TabButtons";
 import PlanetImage from "./components/PlanetImage";
+import PlanetInfo from "./components/PlanetInfo";
 import planetsData from "./data/planetsData";
 
 function App() {
@@ -33,6 +34,11 @@ function App() {
           selectedPlanet={selectedPlanet}
         />
         <PlanetImage selectedPlanet={selectedPlanet} activeTab={activeTab} />
+        <PlanetInfo
+          name={currentPlanet.name}
+          content={currentContent.content}
+          source={currentContent.source}
+        />
       </div>
     </div>
   );
---
+++ src/components/PlanetInfo.jsx
@@ -0,0 +1,30 @@
+import { PADDING } from "../utils/constants";
+import iconSource from "../assets/icon-source.svg";
+
+export default function PlanetInfo({ name, content, source }) {
+  return (
+    <div
+      className="flex flex-col items-center text-center"
+      style={{ paddingInline: PADDING }}
+    >
+      <h1 className="text-white text-preset-3 sm:text-preset-2 md:text-preset-1 mb-4">
+        {name}
+      </h1>
+      <p className="text-preset-11 md:text-preset-8 font-normal text-white mb-6">
+        {content}
+      </p>
+      <div className="text-preset-9 md:text-preset-8 tracking-normal">
+        <span className=" font-normal text-white/50">Source : </span>
+        <a
+          href={source}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-bold text-white/50 underline"
+        >
+          Wikipedia
+          <img src={iconSource} alt="" className="inline ml-1" />
+        </a>
+      </div>
+    </div>
+  );
+}
