c3cd2987aaedb4209eb669958b1fc08420c33db9
anet-eny <anet.eny@gmail.com>
2025-12-22 19:25:34 +0100

    feat: change h1 text to uppercase

--- src/components/PlanetInfo.jsx
+++ src/components/PlanetInfo.jsx
@@ -7,7 +7,7 @@ export default function PlanetInfo({ name, content, source }) {
       className="flex flex-col items-center text-center"
       style={{ paddingInline: PADDING }}
     >
-      <h1 className="text-white text-preset-3 sm:text-preset-2 md:text-preset-1 mb-4">
+      <h1 className="text-white text-preset-3 sm:text-preset-2 md:text-preset-1 uppercase mb-4">
         {name}
       </h1>
       <p className="text-preset-11 md:text-preset-8 font-normal text-white mb-6">
