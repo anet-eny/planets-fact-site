119c9e8d2d05fc5b0a299c390fcaafe6ba7f903e
anet-eny <anet.eny@gmail.com>
2025-12-23 19:13:40 +0100

    feat: update TabButtons layout and color styles

--- src/components/TabButtons.jsx
+++ src/components/TabButtons.jsx
@@ -1,4 +1,5 @@
 import { planetColors } from "../utils/colors";
+import { PADDING } from "../utils/constants";
 
 export default function TabButtons({
   activeTab,
@@ -26,22 +27,23 @@ export default function TabButtons({
 
   return (
     <div
-      className={`flex sm:flex-col justify-evenly border-b border-white/20 sm:border-none ${className}`}
+      className={`flex sm:flex-col justify-evenly sm:gap-2 border-b border-white/20 sm:border-none ${className}`}
+      style={{ paddingInline: PADDING }}
     >
       {tabs.map((tab) => (
         <button
           key={tab.id}
           onClick={() => onTabChange(tab.id)}
-          className={`text-preset-11 uppercase text-white opacity-50 px-2 py-4 cursor-pointer border-y-3 border-transparent  transition-all ${
+          className={`flex gap-4 text-preset-11 uppercase text-white opacity-50 sm:opacity-100 px-2 sm:px-6 py-4 cursor-pointer border-y-3 border-transparent hover:bg-[oklch(0.8822_0_0/_0.2)] transition-all ${
             activeTab === tab.id
-              ? "opacity-100 border-b-3 border-b-(--planet-color) sm:border-none"
-              : "opacity-50 sm:border sm:border-white"
+              ? "opacity-100 border-b-3 border-b-(--planet-color) sm:border sm:border-transparent sm:bg-(--planet-color) "
+              : "opacity-50 sm:border sm:border-white/10 hover:border-transparent"
           }`}
           style={{
             "--planet-color": planetColor,
           }}
         >
-          <span className="hidden sm:inline">{tab.number}</span>
+          <span className="hidden sm:inline opacity-50">{tab.number}</span>
           <span className="sm:hidden">{tab.shortLabel}</span>
           <span className="hidden sm:inline">{tab.label}</span>
         </button>
