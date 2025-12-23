c0ef53b3e884f66d9cb00e23365b2699e8ec9f6e
anet-eny <anet.eny@gmail.com>
2025-12-23 15:34:19 +0100

    fix: change responsive border logic

--- src/components/TabButtons.jsx
+++ src/components/TabButtons.jsx
@@ -6,6 +6,8 @@ export default function TabButtons({
   selectedPlanet,
   className = "",
 }) {
+  const planetColor = planetColors[selectedPlanet.toLowerCase()];
+
   const tabs = [
     { id: "overview", label: "Overview", shortLabel: "Overview", number: "01" },
     {
@@ -22,24 +24,24 @@ export default function TabButtons({
     },
   ];
 
-  const planetColor = planetColors[selectedPlanet.toLowerCase()];
-
   return (
     <div
-      className={`flex justify-evenly border-b border-white/20 ${className}`}
+      className={`flex sm:flex-col justify-evenly border-b border-white/20 sm:border-none ${className}`}
     >
       {tabs.map((tab) => (
         <button
           key={tab.id}
           onClick={() => onTabChange(tab.id)}
-          className={`text-preset-11 uppercase text-white opacity-50 px-2 py-4 cursor-pointer border-y-3 border-transparent transition-all ${
-            activeTab === tab.id ? "opacity-100 border-b-3" : ""
+          className={`text-preset-11 uppercase text-white opacity-50 px-2 py-4 cursor-pointer border-y-3 border-transparent  transition-all ${
+            activeTab === tab.id
+              ? "opacity-100 border-b-3 border-b-(--planet-color) sm:border-none"
+              : "opacity-50 sm:border sm:border-white"
           }`}
           style={{
-            borderBottomColor:
-              activeTab === tab.id ? planetColor : "transparent",
+            "--planet-color": planetColor,
           }}
         >
+          <span className="hidden sm:inline">{tab.number}</span>
           <span className="sm:hidden">{tab.shortLabel}</span>
           <span className="hidden sm:inline">{tab.label}</span>
         </button>
