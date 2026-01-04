5aa7fd01ed2186188a7ffdbf528c95f73c7f7a69
anet-eny <anet.eny@gmail.com>
2026-01-04 20:30:07 +0100

    fix: correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/planet-fact-site/",
+  base: "/planets-fact-site/",
 });
